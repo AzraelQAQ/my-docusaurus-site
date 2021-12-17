be95a473e6261fb2bdc479ec7d1cba640572db99
宇 <oncwnuJddLnjamsGrEGXRq9BeXZk@git.weixin.qq.com>
2021-12-17 16:46:26 +0800

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ module.exports = {
     organizationName: 'facebook', // Usually your GitHub org/user name.
     projectName: 'docusaurus', // Usually your repo name.
     i18n: {
-        defaultLocale: "zh-cmn-Hans",
-        locales: ["zh-cmn-Hans"]
+        defaultLocale: "zh-Hans",
+        locales: ["zh-Hans"]
     },
     themeConfig: {
         navbar: {
