c1c1956aa09fe630c7c05e2b915c5d410767514d
宇 <oncwnuJddLnjamsGrEGXRq9BeXZk@git.weixin.qq.com>
2021-12-21 11:03:06 +0800

    修改语言

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,8 +13,8 @@ module.exports = {
     organizationName: 'facebook', // Usually your GitHub org/user name.
     projectName: 'docusaurus', // Usually your repo name.
     i18n: {
-        defaultLocale: "zh-Hans",
-        locales: ["zh-Hans","en-US"]
+        defaultLocale:'zh-Hans',
+        locales: ['zh-Hans','en-US','fr']
     },
     themeConfig: {
         navbar: {
